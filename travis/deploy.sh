13f79492ea76e404bad55f94a1b7c1683e89b577
cosmin_duna <duna.marius.cosmin@gmail.com>
2020-01-14 12:36:24 +0200

    Update travis.yml

--- travis/deploy.sh
+++
@@ -1,7 +0,0 @@
-git config user.name "radu_coravu";
-git config user.email "radu_coravu@sync.ro";
-git fetch;
-git checkout -f master;
-git reset;
-git commit -m "New release - ${TRAVIS_TAG}";
-git push origin HEAD:master; 
